256e9725e0e915c2cbab1b3b9b2a1601476ebd7d
sungpyo <sungpyo9053@kookmin.ac.kr>
2020-11-20 01:03:35 +0900

    게시판 글 수정

--- myhome/src/main/java/com/godcoder/myhome/model/Board.java
+++ myhome/src/main/java/com/godcoder/myhome/model/Board.java
@@ -1,6 +1,9 @@
 package com.godcoder.myhome.model;
 
+
+import com.sun.istack.NotNull;
 import lombok.Data;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +13,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
+
 @Data
 @Entity
-
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String content;
+
+    public Board() {
+
+    }
 }
